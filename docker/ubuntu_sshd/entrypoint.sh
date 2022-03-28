340cab8b41b7b43fd09b1acdc8db45a9bb13f7f0
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2022-03-28 18:44:16 +0900

    update

--- docker/ubuntu_sshd/entrypoint.sh
+++ docker/ubuntu_sshd/entrypoint.sh
@@ -7,6 +7,7 @@ GROUP_ID=${LOCAL_GID:-9001}
 echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
 useradd -u $USER_ID -o -m $USER_NAME --shell /usr/bin/bash
 groupmod -g $USER_NAME $GROUP_ID
+echo "$USER_NAME:$USER_NAME" | chpasswd
 
 gpasswd -a $USER_NAME sudo
 
--- docker/ubuntu_sshd/make_env.sh
+++ docker/ubuntu_sshd/make_env.sh
@@ -2,6 +2,7 @@
 
 set -eu
 cat <<EOT > .env
+LOCAL_USER=`whoami`
 LOCAL_UID=`id -u`
 LOCAL_GID=`id -g`
 EOT
